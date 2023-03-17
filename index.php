7b3abddb49375f3c595973e5719b19dc38825e54
bhaskar-alagala <38127403+bhaskar-alagala@users.noreply.github.com>
2023-03-18 02:55:51 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 // Create a variable and assign a value to it
-$message = "Hello, world!";
+$message = "Hello, world!<br>";
 
 // Display the value of the variable on the webpage
 echo $message;
-echo "Welcome to chandu projects";
-echo "\nWelcome to Bhaskar projects";
+echo "Welcome to chandu projects<br>";
+echo "\nWelcome to Bhaskar projects<br>";
 ?>
