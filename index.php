d71dac3c6ba2d68d2ae8d217e0e988df009b413e
bhaskar-alagala <38127403+bhaskar-alagala@users.noreply.github.com>
2023-03-30 19:18:11 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 <h1>My Bhsakr  PHP page</h1>
 
 <?php
-echo "Hello World!";
-echo "ADDED THE SECOND LINE IN PHP!";
+echo "Hello World!<br>";
+echo "ADDED THE SECOND LINE IN PHP!<br>";
 ?> 
 
 </body>
